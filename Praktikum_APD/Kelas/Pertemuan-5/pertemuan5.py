d2dc8bc2ac494ebad02dd051d8bf9f72b9eb78f7
ghesyarhegyta <ghesyarhegyta@gmail.com>
2024-10-02 12:08:56 +0800

    Add files via upload

---
+++ Praktikum_APD/Kelas/Pertemuan-5/pertemuan5.py
@@ -0,0 +1,16 @@
+nama = ["celio", "shandy", "farel", "ghazali", "vito"]
+
+print("sebelum: ")
+print(nama)
+print("")
+print("sesudah: ")
+
+# nama.insert(2, "afrizal")
+# print(nama)
+# nama[4]= "fufufafa"
+# hapus = nama.pop(2)
+# print(nama)
+# print(hapus)
+
+
+print(nama[0:2])
